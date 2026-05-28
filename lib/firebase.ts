427a14215c98e445a9849810a9d7c8451874b83e
Kamila Pereira Da Silva Pedroso <kamilabts503@gmail.com>
2026-05-27 20:59:06 -0400

    atualização 2705

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeRN0R4WMl6fMYJBasA1XtHf_GL6ChYUI",
-  authDomain: "aulamobile-2005.firebaseapp.com",
-  databaseURL: "https://aulamobile-2005.firebaseio.com",
-  projectId: "aulamobile-2005",
-  storageBucket: "aulamobile-2005.firebasestorage.app",
-  messagingSenderId: "322588156055",
-  appId: "1:322588156055:web:7d703ab6ff1415a97ac9d2",
-  measurementId: "G-7FVZM3BZ0R"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATA_BASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
